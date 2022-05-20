6fc59991c5ba51b35a31917ad771d3dc62c92b00
Nnamdi Anyanwu <blueking189@gmail.com>
2022-05-21 00:28:15 +0100

    Update simple-printers.c

--- simple-printers.c
+++ simple-printers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_from_to - prints a range of char addresses
- * @start: starting address
- * @stop: stopping address
- * @except: except address
- *
- * Return: number bytes printed
- */
+* print_from_to - prints a range of char addresses
+* @start: starting address
+* @stop: stopping address
+* @except: except address
+*
+* Return: number bytes printed
+*/
 int print_from_to(char *start, char *stop, char *except)
 {
 	int sum = 0;
